1b74dc2d435f139e0b1fd9a76cc891216211312d
Jan Tattermusch <jtattermusch@google.com>
2019-01-29 18:16:58 +0100

    change cert subject

--- tools/internal_ci/windows/grpc_build_packages.bat
+++ tools/internal_ci/windows/grpc_build_packages.bat
@@ -36,7 +36,7 @@ set DOTNET=dotnet
 set SIGNTOOL="c:\Program Files (x86)\Windows kits\10\bin\x86\signtool.exe"
 
 @rem %NUGET% update -self
-%NUGET% sign Grpc.Core.1.18.0.nupkg -CertificateSubjectName "Google Inc" -Verbosity detailed -Timestamper http://timestamp.comodoca.com/authenticode
+%NUGET% sign Grpc.Core.1.18.0.nupkg -CertificateSubjectName "Google LLC" -Verbosity detailed -Timestamper http://timestamp.comodoca.com/authenticode
 
 %SIGNTOOL% 
 
